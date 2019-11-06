0080344d1a68fb0ee64521710a38b4637e10980a
Dionisiaca <claudia.custodiog@gmail.com>
2019-11-06 10:10:39 -0600

    Encode con espacios y minúsculas

--- src/cipher.js
+++ src/cipher.js
@@ -1,15 +1,24 @@
 window.cipher = {
-
-  /*CIFRADO*/
-encode: (offset, userMsg) => {  
-  let upperCaseMsg = userMsg.toUpperCase();
+  encode: (offset, userMsg) => {  
   let encodeMsg= "";
   for (let i = 0; i < userMsg.length; i++) {
-    let result = (upperCaseMsg.charCodeAt(i) - 65 + parseInt(offset)) % 26 + 65;
-    encodeMsg += String.fromCharCode(result);
+    let asciiMsg = userMsg.charCodeAt(i);
+    if (65 <= asciiMsg && asciiMsg <=90) {
+      encodeMsg += String.fromCharCode(
+        ((asciiMsg - 65 + parseInt(offset)) % 26) + 65
+      );
+    } else if (97 <= asciiMsg && asciiMsg <= 122) {
+      encodeMsg += String.fromCharCode(
+        ((asciiMsg - 97 + parseInt(offset)) %26) + 97
+      );
+    } else {
+      encodeMsg += userMsg.charAt(i);
+    }
   }
   return encodeMsg;
+  console.log(encodeMsg, "encodeMsg");
 },
+
   /*DESCIFRADO*/
   decode: (offset, userMsg) => {
   let upperCaseMsg = userMsg.toUpperCase();
@@ -22,3 +31,13 @@ encode: (offset, userMsg) => {
   }
 
 }
+
+  /*CIFRADO*/
+/*encode: (offset, userMsg) => {  
+  let upperCaseMsg = userMsg.toUpperCase();
+  let encodeMsg= "";
+  for (let i = 0; i < userMsg.length; i++) {
+    let result = (upperCaseMsg.charCodeAt(i) - 65 + parseInt(offset)) % 26 + 65;
+    encodeMsg += String.fromCharCode(result);
+  }
+  return encodeMsg;*/
