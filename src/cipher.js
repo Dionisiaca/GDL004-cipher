d99e224f4aa0d0716b35887697ba2cec93335b7e
Dionisiaca <claudia.custodiog@gmail.com>
2019-11-06 10:39:00 -0600

    Decodificar con mayúsculas, minúsculas y espacios

--- src/cipher.js
+++ src/cipher.js
@@ -1,4 +1,5 @@
 window.cipher = {
+ /*CIFRADO*/ 
   encode: (offset, userMsg) => {  
   let encodeMsg= "";
   for (let i = 0; i < userMsg.length; i++) {
@@ -9,35 +10,31 @@ window.cipher = {
       );
     } else if (97 <= asciiMsg && asciiMsg <= 122) {
       encodeMsg += String.fromCharCode(
-        ((asciiMsg - 97 + parseInt(offset)) %26) + 97
+        ((asciiMsg - 97 + parseInt(offset)) % 26) + 97
       );
     } else {
       encodeMsg += userMsg.charAt(i);
     }
   }
   return encodeMsg;
-  console.log(encodeMsg, "encodeMsg");
-},
 
+},
   /*DESCIFRADO*/
   decode: (offset, userMsg) => {
-  let upperCaseMsg = userMsg.toUpperCase();
-  let decodeMsg= "";
+  let decodeMsg = "";
   for (let i = 0; i < userMsg.length; i++) {
-    let result = (upperCaseMsg.charCodeAt(i) - 90 - parseInt(offset)) % 26 + 90;
-    decodeMsg += String.fromCharCode(result);
+    let asciiMsg = userMsg.charCodeAt(i);
+    if (65 <= asciiMsg && asciiMsg <= 90){
+      decodeMsg += String.fromCharCode(
+        ((asciiMsg + 65 - parseInt(offset)) % 26) + 65
+      );
+    } else if (97 <= asciiMsg && asciiMsg <= 122){
+      decodeMsg += String.fromCharCode(
+        ((asciiMsg - 97 - parseInt(offset) + 52) % 26) + 97
+      );
+    } else {
+      decodeMsg += userMsg.charAt(i);
+    }
   }
   return decodeMsg;
-  }
-
-}
-
-  /*CIFRADO*/
-/*encode: (offset, userMsg) => {  
-  let upperCaseMsg = userMsg.toUpperCase();
-  let encodeMsg= "";
-  for (let i = 0; i < userMsg.length; i++) {
-    let result = (upperCaseMsg.charCodeAt(i) - 65 + parseInt(offset)) % 26 + 65;
-    encodeMsg += String.fromCharCode(result);
-  }
-  return encodeMsg;*/
+}}
